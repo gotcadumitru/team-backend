75c23f06cb4b50a18fc81dc060610b3c7b55ac73
Dumitru gotca <dum.gotca@gmail.com>
2022-04-04 19:48:05 +0300

    add websocket

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const app = express();
 const authRouter = require('./routes/auth');
 const postRouter = require('./routes/post');
 const fileRouter = require('./routes/files');
+const conversationRoute = require('./routes/conversation');
+const messageRoute = require('./routes/message');
 
 const port = process.env.PORT || 8080;
 
@@ -32,6 +34,61 @@ connection.once('open', () => {
 app.use('/api/auth', authRouter);
 app.use('/api/post', postRouter);
 app.use('/api/files', fileRouter);
+app.use('/api/files', fileRouter);
+app.use("/api/conversation", conversationRoute);
+app.use("/api/message", messageRoute);
+
+
+//websocket
+const io = require("socket.io")(8900, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+
+let users = [];
+
+const addUser = (userId, socketId) => {
+  !users.some((user) => user.userId === userId) &&
+    users.push({ userId, socketId });
+};
+
+const removeUser = (socketId) => {
+  users = users.filter((user) => user.socketId !== socketId);
+};
+
+const getUser = (userId) => {
+  return users.find((user) => user.userId === userId);
+};
+
+io.on("connection", (socket) => {
+  //when connect
+  console.log("a user connected.");
+
+  //take userId and socketId from user
+  socket.on("addUser", (userId) => {
+    addUser(userId, socket.id);
+    io.emit("getUsers", users);
+  });
+
+  //send and get message
+  socket.on("sendMessage", ({ senderId, receiverId, text }) => {
+    const user = getUser(receiverId);
+    io.to(user.socketId).emit("getMessage", {
+      senderId,
+      text,
+    });
+  });
+
+  //when disconnect
+  socket.on("disconnect", () => {
+    console.log("a user disconnected!");
+    removeUser(socket.id);
+    io.emit("getUsers", users);
+  });
+});
+
+
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
