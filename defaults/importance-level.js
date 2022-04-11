299885d7a3b333b3099a5f44fc251afa3e07dbdf
gotcadumitru <gotca.dumitru@mail.com>
2022-04-11 19:33:44 +0300

    add routes for posts

---
+++ defaults/importance-level.js
@@ -0,0 +1,20 @@
+const IMPORTANCE_LEVEL = [
+  {
+    id: 0,
+    name: "local"
+  },
+  {
+    id: 1,
+    name: "regional"
+  },
+  {
+    id: 2,
+    name: "national"
+  },
+  {
+    id: 3,
+    name: "international"
+  },
+
+]
+module.exports = IMPORTANCE_LEVEL
---
+++ defaults/priority-level.js
@@ -0,0 +1,25 @@
+const PRIORITY_LEVEL = [
+  {
+    id: 0,
+    name: "foarte important"
+  },
+  {
+    id: 1,
+    name: "important"
+  },
+  {
+    id: 2,
+    name: "mediu"
+  },
+  {
+    id: 3,
+    name: "mai putin important"
+  },
+  {
+    id: 4,
+    name: "nesemnificativ"
+  },
+
+
+]
+module.exports = PRIORITY_LEVEL
