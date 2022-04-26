c814055c67ba4243c2d97637dce887b84965f687
Dumitru Gotca <dumitru.gotca@assist.ro>
2022-04-26 23:58:38 +0300

    posts get refactoring

--- utils/utils.post.js
+++ utils/utils.post.js
@@ -1,7 +1,20 @@
 const File = require('../models/model.file');
+const User = require('../models/model.user');
+const { getUserSmallType } = require('./utils.user');
 
-const getPostFullType = async (user) => {
-
+const getPostFullType = async (post, users = {}) => {
+    let postFullType = {
+        ...post
+    }
+    postFullType.files = await File.find({
+        idFromDrive: { $in: post.files },
+    });
+    if (users[post.author]) {
+        postFullType.author = users[post.author];
+    } else {
+        postFullType.author = await getUserSmallType(await User.findById(post.author));
+    }
+    return postFullType
 };
 
 module.exports = { getPostFullType };
