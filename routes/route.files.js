63544488780952b9c06751d6049ba80ba8740938
Dragos Todoroșcean <dragostodoroscean@Dragoss-Assist-MacBook-Pro.local>
2022-05-03 12:48:28 +0300

    Get file url after upload

--- routes/route.files.js
+++ routes/route.files.js
@@ -1,45 +1,46 @@
 const checkToken = require("./verifyToken");
 // const File = require("../models/model.file");
-const multer = require('multer')
+const multer = require("multer");
 const upload = multer({
-  storage: multer.memoryStorage()
-})
+  storage: multer.memoryStorage(),
+});
 
 const router = require("express").Router();
-const {
-  bucket
-} = require("../utils/utils.googleDrive");
+const { bucket } = require("../utils/utils.googleDrive");
 
-
-
-
-router.post("/upload", upload.any('files'), async (req, res) => {
+router.post("/upload", upload.any("files"), async (req, res) => {
   try {
     if (!req.files) {
-      res.status(400).send("Error: No files found")
+      res.status(400).send("Error: No files found");
     } else {
       req.files.map(async (file) => {
-
-        const blob = bucket.file(file.originalname)
+        const blob = bucket.file(file.originalname);
 
         const blobWriter = blob.createWriteStream({
           metadata: {
-            contentType: file.mimetype
-          }
-        })
+            contentType: file.mimetype,
+          },
+          predefinedAcl: "publicRead",
+        });
 
-        blobWriter.on('error', (err) => {
-          console.log(err)
-        })
+        blobWriter.on("error", (err) => {
+          console.log(err);
+        });
 
-        blobWriter.on('finish', async () => {
-          const url = await blob.getSignedUrl()
-          console.log(url)
-          res.status(200).send("File uploaded.")
-        })
+        blobWriter.on("finish", () => {
+          blob
+            .getSignedUrl({
+              action: "read",
+              expires: "03-09-2491",
+            })
+            .then((signedUrls) => {
+              console.log(signedUrls[0]);
+            });
+          res.status(200).send("File uploaded.");
+        });
 
-        blobWriter.end(file.buffer)
-      })
+        blobWriter.end(file.buffer);
+      });
     }
   } catch (err) {
     console.log(err);
